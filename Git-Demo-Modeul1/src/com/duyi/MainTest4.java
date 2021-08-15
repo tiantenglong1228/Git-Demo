cb30ab67651f0b14ae1c38a90fe56c20b563c51c
tiantenglong1228 <381689489@qq.com>
2021-08-15 10:39:55 +0800

    尝试进行第六次更改

---
+++ Git-Demo-Modeul1/src/com/duyi/MainTest4.java
@@ -0,0 +1,11 @@
+package com.duyi;
+
+public class MainTest4 {
+
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+    }
+
+}
